8e9f9cae3d0a6ab8da507d56dcb11852bbf85b2b
Matt Carter <m@ttcarter.com>
2023-08-10 09:34:46 +1000

    task: Basic storybook setup + example <dump/> component

---
+++ components/dump.stories.js
@@ -0,0 +1,23 @@
+import Dump from './dump.vue';
+
+export default {
+	title: 'Debugging/Dump',
+	component: Dump,
+	tags: ['autodocs'],
+}
+
+export const Object = {
+	args: {
+		value: {foo: 1, bar: 2, baz: 3},
+	},
+}
+
+export const Collection = {
+	args: {
+		value: [
+			{title: 'Foo widget', value: 123},
+			{title: 'Bar widget', value: 456},
+			{title: 'Baz widget', value: 789},
+		],
+	},
+}
---
+++ components/dump.vue
@@ -0,0 +1,17 @@
+<script>
+/**
+* Basic data dumping + JSON formatting as a Vue component
+*
+* @param {Object|Array} value The value to output
+*/
+/* eslint-disable vue/html-indent */
+export default {
+	props: {
+		value: {type: [Object, Array]},
+	},
+}
+</script>
+
+<template>
+<pre>{{value}}</pre>
+</template>
