c3e7ded3346d703908409541e96d341b9d1b4ac9
Matt Carter <m@ttcarter.com>
2023-09-06 16:25:45 +1000

    feat: <pagination/>

---
+++ components/pagination.stories.js
@@ -0,0 +1,15 @@
+import Pagination from '#components/pagination';
+
+export default {
+	title: 'Components/Pagination',
+	component: Pagination,
+	tags: ['autodocs'],
+}
+
+export const Example = {
+	args: {
+		min: 1,
+		max: 100,
+		value: 50,
+	},
+}
