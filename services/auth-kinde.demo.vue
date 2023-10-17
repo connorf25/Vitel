ae2cbd4f3411d4f168dd124e454c2b990ed2eaba
Matt Carter <m@ttcarter.com>
2023-10-17 15:46:35 +1000

    fix: Wrong Kinde domain suffix

--- services/auth-kinde.demo.vue
+++ services/auth-kinde.demo.vue
@@ -16,7 +16,7 @@ export default {
 		<div class="card mb-2">
 			<div class="card-header">Kinde Auth</div>
 			<div class="card-body">
-				<p>A Vue wrapper around the <a href="https://kinde.io" target="_blank">Kinde.io</a> platform to provide basic auth and user information</p>
+				<p>A Vue wrapper around the <a href="https://kinde.io" target="_blank">Kinde.com</a> platform to provide basic auth and user information</p>
 				<div class="list-group">
 					<a
 						@click="$kinde.promise()"
